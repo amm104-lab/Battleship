f5aa40a00e4c3c91dc79bc44e969af32db3fc86a
amm <180281358+amm104-lab@users.noreply.github.com>
2024-12-09 05:51:01 +0100

    declared empty factories and commented future usage

---
+++ factories.js
@@ -0,0 +1,11 @@
+function createShip(){
+    //length, times hit, sunk, hit increase
+}
+
+function createGameboard(){
+    //places ships via factory, receiveAttack to determine if ship was hit, report if all ships have been sunk
+}
+
+function createPlayer(){
+    //diff in real and com, own gameboard
+}

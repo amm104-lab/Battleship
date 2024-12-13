25ab7973448320cb62a9f4186910395ff6995bad
amm <amm@tutao.de>
2024-12-13 07:48:18 +0100

    Added functions to create main div aswell as playerBoards

--- dom.js
+++ dom.js
@@ -1,3 +1,33 @@
-function setUp(){
+function setUpContainer(){
 
+    const body = document.querySelector("body")
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+
+    body.appendChild(container);
+}
+
+function setUpPlayerBoard(player) {
+
+    const boardBase = document.createElement("div");
+    boardBase.setAttribute("class", "board");
+
+    const container = document.querySelector(".container");
+    container.appendChild(boardBase);
+
+    for(let i = 0; i < player.getPlayerBoard().length; i++){
+
+        const coord = document.createElement("div");
+        coord.setAttribute("class", "coord");
+        coord.setAttribute("data-index", `${i}`);
+        boardBase.appendChild(coord);
+
+        for(let j = 0; j < player.getPlayerBoard()[i].length; j++){
+
+            const inate = document.createElement("div");
+            inate.setAttribute("class", "inate");
+            inate.setAttribute("data-index", `${j}`);
+            boardBase.appendChild(inate);
+        }
+    }
 }
